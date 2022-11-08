09ef6355bd02127130c40419b3e016e4d2f40589
Andreia <andreiacpafonso@gmail.com>
2022-11-08 14:32:06 +0000

    crud

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -7,12 +7,13 @@ const app = require("../app");
 const bcrypt = require("bcryptjs");
 const { isLoggedIn } = require("../middleware/route-guard");
 const uploader = require("../middleware/cloudinary.config.js");
+const { findByIdAndDelete } = require("../models/User.model");
 
 router.get("/profile", isLoggedIn, async (req, res) => {
   const currentUser = await User.findById(req.session.user._id).populate(
     "userPizza"
   );
-  console.log(currentUser);
+  
   res.render("profile", { currentUser, isConnected: true });
 });
 router.get("/register-pizza", isLoggedIn, (req, res) => {
@@ -23,9 +24,17 @@ router.post(
   uploader.single("imageUrl"),
   async (req, res, next) => {
     // the uploader.single() callback will send the file to cloudinary and get you and obj with the url in return
-    console.log("file is: ", req.file);
+    let pizzaImg 
+     
+    if (req.file == undefined){
+      pizzaImg = "https://img.freepik.com/free-photo/pizza-with-salami-tomatoes-olives-cheese-dough-with-whole-wheat-flour-italian-food_2829-6855.jpg?w=1380&t=st=1667915693~exp=1667916293~hmac=bf0f0ed1b04c6aa61987760ee4c701f5940a2cb342e3582cc2bcdc1f8f8be842"
+    }
+    else { pizzaImg = req.file.path}
+    
+    
+    
     const newPizza = await Userpizza.create({
-      imageUrl: req.file.path,
+      imageUrl: pizzaImg,
       namePizza: req.body.namePizza,
       city: req.body.city,
       country: req.body.country,
@@ -35,15 +44,37 @@ router.post(
     const currentUser = await User.findByIdAndUpdate(req.session.user._id, {
       $push: { userPizza: newPizza._id },
     });
+  
 
-    if (!req.file) {
-      console.log("there was an error uploading the file");
-      next(new Error("No file uploaded!"));
-      return;
-    }
     res.redirect("/profile");
     // You will get the image url in 'req.file.path'
     // Your code to store your url in your database should be here
   }
 );
+
+router.get("/update/:id", async (req, res) => {
+  
+  const updatePizza = await Userpizza.findById(req.params.id)
+  const currentUser = await User.findById(req.session.user._id)
+ 
+res.render('update',  {updatePizza, currentUser} )
+})
+
+//  router.post("/update/:Id"
+
+router.post("/update/:id",  uploader.single("imageUrl"), async (req,res)  => {
+  console.log (req.body, req.file)
+  await Userpizza.findByIdAndUpdate(req.params.id, {...req.body,ingredients: req.body.ingredients.split(' ')})
+  res.redirect("/profile");
+  
+})
+
+router.get("/delete/:id", async (req, res) => {
+  
+    await Userpizza.findByIdAndDelete(req.params.id)
+ 
+ 
+res.redirect ('/profile')
+})
+
 module.exports = router;
